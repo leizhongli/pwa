a29ec6a1b1040c1404a5af7e55faf202fc9aedad
God <God@ACA801F3.ipt.aol.com>
2018-11-09 09:31:11 +0800

    html

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,41 @@
-var cacheName = 'helloworld';
+var cacheName = 'latestNews-v1';
 
+// Cache our known resources during install
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => cache.addAll([
-                './public/image/figur.jpg'
+                './public/js/script.js',
+                './index.html'
             ]))
-    )
-})
+    );
+});
+
+// Cache any new resources as they are fetched
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request) 
+        caches.match(event.request, { ignoreSearch: true })
             .then(function (response) {
                 if (response) {
                     return response;
                 }
-                var requestToCache = event.request.clone();
-                return fetch(requestToCache.url)
-                        .then(function(response) {
-                            if (!response || response.status!=200) {
-                                return response;
-                            }
-                            var responseToCache = response.clone();
-                            caches.open(cacheName)
-                                .then(function(cache) {
-                                    cache.put(requestToCache.url, responseToCache)
-                                })
+                var fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest).then(
+                    function (response) {
+                        if (!response || response.status !== 200) {
                             return response;
-                        })
+                        }
+
+                        var responseToCache = response.clone();
+                        caches.open(cacheName)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
             })
-    )
-})
+    );
+});
