fa567a93f2ec71ec33d560770f02d74f48efa057
leizhongli <lzlcyb1010@163.com>
2018-11-14 09:54:13 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,49 @@
 "use strict";
-
+var cacheStorageKey = 'helloworld';
+var cacheList = [
+  './public/image/figur.jpg'
+];
 self.addEventListener('install', event => {
-  self.skipWaiting();
-
-  event.waitUntil(
-    // caching etc
-  );
+  console.log('Cache event!');
+  e.waitUntil(
+    caches.open(cacheStorageKey).then(function(cache) {
+      console.log('Adding to Cache:', cacheList)
+      return cache.addAll(cacheList)
+    }).then(function() {
+      console.log('install event open cache ' + cacheStorageKey);
+      console.log('Skip waiting!')
+      return self.skipWaiting();
+    })
+  )
 });
+self.addEventListener('activate', function(e) {
+  console.log('Activate event');
+  console.log('Promise all', Promise, Promise.all);
+  // active事件中通常做一些过期资源释放的工作
+  var cacheDeletePromises = caches.keys().then(cacheNames => {
+    console.log('cacheNames', cacheNames, cacheNames.map);
+    return Promise.all(cacheNames.map(name => {
+      if (name !== cacheStorageKey) { // 如果资要缓存的key不同则源的key与当前需释放资源
+        console.log('caches.delete', caches.delete);
+        var deletePromise = caches.delete(name);
+        console.log('cache delete result: ', deletePromise);
+        return deletePromise;
+      } else {
+        return Promise.resolve();
+      }
+    }));
+  });
+
+  console.log('cacheDeletePromises: ', cacheDeletePromises);
+  e.waitUntil(
+    Promise.all([cacheDeletePromises]
+    ).then(() => {
+      console.log('activate event ' + cacheStorageKey);
+      console.log('Clients claims.')
+      return self.clients.claim();
+    })
+  )
+})
 // Listen to fetch events
 self.addEventListener('fetch', function (event) {
 
